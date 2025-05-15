5fbf3505f72fbfd9459169a540abe8c0ab0146d8
Bob <uncoolbob@gmail.com>
2025-05-15 14:19:15 +0100

    profileSet merging seems done

--- R/parse-spreadsheet.R
+++ R/parse-spreadsheet.R
@@ -10,7 +10,7 @@ library(knitr)
 #' @param filePath Path to .xlsx file
 #' @param allData a list to accumulate all the data (see `processExperiment` for info on its structure)
 #' @param process_fn A function to call on each parsed block:
-#'                   function(allData, meta_tbl, data_tbl, units_map)
+#'                   function(allData, meta_map, data_tbl, units_map)
 #' @return allData accumulated data from passing through `process_fn`
 parseSpreadsheet <- function(filePath, allData, process_fn) {
   if (!file.exists(filePath)) {
@@ -20,10 +20,10 @@ parseSpreadsheet <- function(filePath, allData, process_fn) {
     stop("You must supply a `data` arg that is a list")
   }
   if (missing(process_fn) || !is.function(process_fn)) {
-    stop("You must supply a process_fn(meta_tbl, data_tbl, units_map) function.")
+    stop("You must supply a process_fn(meta_map, data_tbl, units_map) function.")
   }
   
-  sheets <- c('TriTrypDB')   # excel_sheets(filePath)
+  sheets <- c('PlasmoDB')   # excel_sheets(filePath)
   for (sheetName in sheets) {
     message("Processing sheet: ", sheetName)
     raw <- read_excel(filePath,
@@ -55,9 +55,8 @@ parseSpreadsheet <- function(filePath, allData, process_fn) {
         str_remove(":$") %>%
         str_trim()
       vals <- block[meta_rows, 2] %>% pull() %>% str_trim()
-      meta_tbl <- tibble(key = keys, value = vals) %>%
-        pivot_wider(names_from = key, values_from = value)
-      
+      meta_map <- as.list(setNames(vals, keys))
+
       # extract data between header and units
       data_start <- header_row + 1
       data_end   <- unit_row - 1
@@ -88,36 +87,54 @@ parseSpreadsheet <- function(filePath, allData, process_fn) {
       } else {
         units_map <- setNames(character(0), character(0))
       }
+      units_map <- as.list(units_map)
       
       # hand off to user-provided function to user-provided function
-      allData <- process_fn(allData, meta_tbl, data_tbl, units_map)
+      allData <- process_fn(allData, meta_map, data_tbl, units_map)
     }
   }
   
   return(invisible(allData))
 }
 
-processExperiment <- function(allData, meta_tbl, data_tbl, units_map) {
-  key <- paste(meta_tbl$speciesAndStrain, meta_tbl$datasetName, sep="/")
+processExperiment <- function(allData, meta_map, data_tbl, units_map) {
+  key <- paste(meta_map$speciesAndStrain, meta_map$datasetName, sep="/")
   message(glue::glue("Processing {key} ..."))
   if (allData %>% hasName(key)) {
     message(glue::glue("Appending to {key} ..."))
-    # now we need to merge the data and check that the units are the same
-    # we should also append `meta$profileSetName` with the extra profileSetName
+    # now we need to merge the data
+    # first let's check that the units are the same
     prev <- allData[[key]]
     # units check is the simplest
     if (!identical(prev$units, units_map)) {
       cat(
-        "Units from", meta_tbl$profileSetName, ":\n"
+        "Units from", meta_map$profileSetName, ":\n"
       )
       print(units_map)
-      cat("are not identical to previous profileSets", prev$meta_tbl$profileSetName, ":\n")
+      cat("are not identical to previous profileSets", prev$meta_map$profileSetName, ":\n")
       print(prev$units)
       stop(glue::glue("Aborting due to unit mismatch for {key}"), call. = FALSE)
     }
+    # we should also append `meta$profileSetName` with the extra profileSetName
+    prev$meta$profileSetName <- c(prev$meta$profileSetName, meta_map$profileSetName)
+
+    # and now check the data column names and types (via a vec_ptype prototype) are the same
+    if (!identical(vctrs::vec_ptype(prev$data), vctrs::vec_ptype(data_tbl))) {
+      print("Columns missing: ", setdiff(names(prev$data), names(data_tbl)))
+      print("Columns extra: ", setdiff(names(data_tbl), names(prev$data)))
+      stop(glue::glue("Aborting due to data columns mismatch for {key}"), call. = FALSE)
+    }
+    # now bind_rows but watch out for duplicate rows
+    unioned <- union(prev$data, data_tbl)
+    if (unioned %>% pull('sample ID') %>% anyDuplicated() > 0) {
+      stop(glue::glue("Aborting due duplicate 'sample ID' after data union for {key}"), call. = FALSE)
+    }
+    prev$data <- unioned
+    
+    allData[[key]] <- prev
   } else {
     allData[[key]] <- list(
-      meta = meta_tbl,
+      meta = meta_map,
       data = data_tbl,
       units = units_map
     )
@@ -125,6 +142,5 @@ processExperiment <- function(allData, meta_tbl, data_tbl, units_map) {
   return(allData)
 }
 
-allData <- list()
-parseSpreadsheet('../data/third-reannotation-gpt-4.1-nomissing.xlsx', allData, processExperiment)
+allData <- parseSpreadsheet('../data/RNA-Seq sample re-annotation for QC.xlsx', list(), processExperiment)
 
